3b195a2d832ae475f7526a519e43bfc4f73da82b
mrmrdd <bpotvora@yandex.ru>
2016-11-15 18:09:09 +0200

    Created jUnit test for UserService

---
+++ src/test/java/com/ak/UserServiceTest.java
@@ -0,0 +1,30 @@
+package com.ak;
+
+import com.ak.entity.User;
+import com.ak.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by MacDuck on 11/15/16.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ContactBookApplication.class)
+public class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+
+
+    @Test
+    public void addUserTest(){
+        User user1 = new User("admin", "qwerty", "EKA");
+        userService.addUser(user1);
+        assertEquals("admin", userService.findByLogin("admin").getLogin());
+    }
+}
